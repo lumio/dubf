63ca2419a7f54d5a0c8509619c89ddc81695a816
Benjamin Falk <benjamin.falk@cyanjoe.com>
2016-10-21 17:55:59 +0200

    build main.css

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,23 @@
 'use strict';
 
+const path = require( 'path' );
 const gulp = require( 'gulp' );
-gulp.task( 'build', function () {
+const sourcemaps = require( 'gulp-sourcemaps' );
+const autoprefixer = require( 'gulp-autoprefixer' );
+const concat = require( 'gulp-concat' );
+
+gulp.task( 'build-css', function () {
+  gulp.src( path.join( __dirname, './public/css' ) + '/**/*.css' )
+    .pipe( sourcemaps.init() )
+    .pipe( autoprefixer( {
+      browsers: [ 'last 2 versions' ],
+      cascade: false,
+    } ) )
+    .pipe( concat( 'main.css' ) )
+    .pipe( sourcemaps.write( '.' ) )
+    .pipe( gulp.dest( 'public_dist/css' ) );
+} );
+
+gulp.task( 'build', [ 'build-css' ], function () {
 
 } );
