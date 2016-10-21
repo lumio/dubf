6b09ca8e2a7056904cd18e26195eb74718a6d9b8
Benjamin Falk <benjamin.falk@cyanjoe.com>
2016-10-21 21:40:15 +0200

    replace source files to concated version

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,14 @@ const sourcemaps = require( 'gulp-sourcemaps' );
 const autoprefixer = require( 'gulp-autoprefixer' );
 const concat = require( 'gulp-concat' );
 const cleanCSS = require( 'gulp-clean-css' );
+const order = require( 'gulp-order' );
+const htmlreplace = require( 'gulp-html-replace' );
+
+const publicFolder = path.join( __dirname, './public' );
+const distFolder = path.join( __dirname, './public_dist' );
 
 gulp.task( 'build-css', function () {
-  gulp.src( path.join( __dirname, './public/css' ) + '/**/*.css' )
+  gulp.src( path.join( publicFolder, './css/**/*.css' ) )
     .pipe( sourcemaps.init() )
     .pipe( autoprefixer( {
       browsers: [ 'last 2 versions' ],
@@ -17,10 +22,30 @@ gulp.task( 'build-css', function () {
     .pipe( concat( 'main.css' ) )
     .pipe( cleanCSS() )
     .pipe( sourcemaps.write( '.' ) )
-    .pipe( gulp.dest( 'public_dist/css' ) );
+    .pipe( gulp.dest( path.join( distFolder, 'css' ) ) );
 } );
 
-gulp.task( 'build', [ 'build-css' ], function () {
-  gulp.src( path.join( __dirname, './public/images/**/*' ) )
-    .pipe( gulp.dest( 'public_dist/images' ) );
+gulp.task( 'build-js', function () {
+  gulp.src( path.join( publicFolder, './js/**/*.js' ) )
+    .pipe( order( [
+        'lite.js',
+        'lite-request.js',
+        'view.js',
+        'app.js'
+      ]
+    ) )
+    .pipe( concat( 'app.js' ) )
+    .pipe( gulp.dest( path.join( distFolder, 'js' ) ) );
+} );
+
+gulp.task( 'build', [ 'build-js', 'build-css' ], function () {
+  gulp.src( path.join( publicFolder, './images/**/*' ) )
+    .pipe( gulp.dest( path.join( distFolder, 'images' ) ) );
+
+  gulp.src( path.join( publicFolder, './index.html' ) )
+    .pipe( htmlreplace( {
+      css: 'css/main.css',
+      js: 'js/app.js'
+    } ) )
+    .pipe( gulp.dest( distFolder ) );
 } );
