404d7cb7f67c6d2021030b9d49dcdd5c9e48500c
Benjamin Falk <benjamin.falk@cyanjoe.com>
2016-10-21 18:12:05 +0200

    minify css and copy images

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const gulp = require( 'gulp' );
 const sourcemaps = require( 'gulp-sourcemaps' );
 const autoprefixer = require( 'gulp-autoprefixer' );
 const concat = require( 'gulp-concat' );
+const cleanCSS = require( 'gulp-clean-css' );
 
 gulp.task( 'build-css', function () {
   gulp.src( path.join( __dirname, './public/css' ) + '/**/*.css' )
@@ -14,10 +15,12 @@ gulp.task( 'build-css', function () {
       cascade: false,
     } ) )
     .pipe( concat( 'main.css' ) )
+    .pipe( cleanCSS() )
     .pipe( sourcemaps.write( '.' ) )
     .pipe( gulp.dest( 'public_dist/css' ) );
 } );
 
 gulp.task( 'build', [ 'build-css' ], function () {
-
+  gulp.src( path.join( __dirname, './public/images/**/*' ) )
+    .pipe( gulp.dest( 'public_dist/images' ) );
 } );
