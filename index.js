d9b91484159b65e1158fbbdde1843b7656da3220
Benjamin Falk <benjamin.falk@cyanjoe.com>
2016-10-21 20:17:54 +0200

    better error message when config.js is missing or port is in use

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 'use strict';
 
+const colors = require( 'colors' );
 const path = require( 'path' );
 const hapi = require( 'hapi' );
-const config = require( path.join( __dirname, 'config.js' ) );
+let config;
+try {
+  config = require( path.join( __dirname, 'config.js' ) );
+} catch ( err ) {
+  if ( err.code && err.code === 'MODULE_NOT_FOUND' ) {
+    console.error( '\nWHOUPS!'.yellow );
+    console.error( '  |'.yellow );
+    console.error( '  v\n'.yellow );
+    console.error( 'It seams that you have no config.js. Duplicate config.example.js and name it config.js to get started.' );
+    console.error( 'In order to get this project working, you need to register on <http://themoviedb.org> and get yourself an API v3 key' );
+  }
+  else
+    console.error( err );
+
+  process.exit( 1 );
+}
 
 let staticDirectory = 'public_dist';
 if ( process.env.DEVELOPMENT ) {
@@ -53,7 +69,18 @@ server.register( require( 'inert' ), ( err ) => {
   server.start( ( err ) => {
 
     if ( err ) {
-      throw err;
+      switch ( err.errno ) {
+        case 'EADDRINUSE':
+          console.error( '\nWHOUPS!'.yellow );
+          console.error( '  |'.yellow );
+          console.error( '  v\n'.yellow );
+          console.error( `It seams that the port ${server.info.port} is already in use.\nChange the port number in your config.js and try again.` );
+          break;
+        default:
+          throw err;
+      }
+
+      process.exit( 1 );
     }
 
     console.log( 'Server running at ', server.info.uri );
